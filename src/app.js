5706e166e5b4c0c6cbab48a7aa9d03977eebd0ed
gill3hh <pg19uy@brocku.ca>
2024-09-20 00:54:57 -0400

    dir

--- src/app.js
+++ src/app.js
@@ -26,6 +26,8 @@ app.use(cookieparser())
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'Home_page')));
+
 
 
 //routes import
